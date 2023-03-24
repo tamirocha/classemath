349c417e66e42c354b366531da136069234be2f1
Tamiris <tm.araujo@yahoo.com.br>
2023-03-24 14:56:05 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.max;
+
+public class Main {
+
+    //static final double PI = 3.1415;   // PI é uma constante, valor não mudará. A classe Math já tem essa constante pronta.
